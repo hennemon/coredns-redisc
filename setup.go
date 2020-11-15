48e2788bf6459c323c5b9f2b539a2c64b432547d
hennemon <h3nn3m0n@gmail.com>
2020-11-15 16:00:17 +0100

    fixed wrong caddy import

--- setup.go
+++ setup.go
@@ -13,7 +13,7 @@ import (
 	"github.com/coredns/coredns/plugin/metrics"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 )
 
 var log = clog.NewWithPlugin("redisc")
